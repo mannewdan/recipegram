49b08ee48d7be1e2d01a020bb224036d5cf52bef
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-01-26 22:38:46 -0600

    search indicator

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -9,9 +9,19 @@ export default function Search() {
     <>
       <SearchBar search={search} />
 
-      {isGathering && <span>Searching...</span>}
+      {isGathering && (
+        <div className="search-indicator">
+          <span>Searching...</span>
+        </div>
+      )}
 
-      {!isGathering && (
+      {!isGathering && recipes.length === 0 && (
+        <div className="search-indicator">
+          <span>Discover new recipes</span>
+        </div>
+      )}
+
+      {!isGathering && recipes.length > 0 && (
         <div className="recipe-container">
           {recipes.map((r) => {
             return <Recipe key={r.id} recipe={r} />;
